0fa1ac2e08e8a544df40c44536afcf0d89a970d8
Giuseppe <giuseppe@giuseppe-davies.co.uk>
2012-07-24 13:53:53 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,64 @@
+$(function(){
+
+	new function($) {
+	  $.fn.setCursorPosition = function(pos) {
+		if ($(this).get(0).setSelectionRange) {
+		  $(this).get(0).setSelectionRange(pos, pos);
+		} else if ($(this).get(0).createTextRange) {
+		  var range = $(this).get(0).createTextRange();
+		  range.collapse(true);
+		  range.moveEnd('character', pos);
+		  range.moveStart('character', pos);
+		  range.select();
+		}
+	  }
+	}(jQuery);
+
+	$.support.placeholder = (function(){
+		var input = document.createElement('input');
+		return 'placeholder' in input;
+	})();
+	
+	if (!$.support.placeholder){
+		
+		$("input[type=text]").each(function(){
+			
+			var $this = $(this);
+			var placeholder = $this.attr("placeholder");
+			
+			$this.val(placeholder).addClass("placeholderText");
+			
+			$this.focus(function(){
+				if($this.val() == placeholder){
+					
+					if ($this.get(0).setSelectionRange) {
+						$this.get(0).setSelectionRange(0, 0);
+					} else if ($this.get(0).createTextRange) {
+						var range = $this.get(0).createTextRange();
+						range.collapse(true);
+						range.moveEnd('character', 0);
+						range.moveStart('character', 0);
+						range.select();
+					}
+					
+				}
+			});
+			$this.blur(function(){
+				if($this.val() == ""){
+					$this.val(placeholder).addClass("placeholderText");
+				}
+			});
+			$this.keypress(function(e){
+				if($this.val() == placeholder){
+					$this.val("").removeClass("placeholderText");
+				}
+			});
+			$this.keyup(function(){
+				if($this.val() == ""){
+					$this.val(placeholder).addClass("placeholderText");
+				}
+			});	
+			
+		});
+	}
+});
