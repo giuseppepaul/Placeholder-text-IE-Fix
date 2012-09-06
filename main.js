7b8a2a22af07cb89b902b5d1e2de0e1dfd08e1d0
Giuseppe <giuseppe@giuseppe-davies.co.uk>
2012-09-06 09:08:11 +0100

    Removed unused function

--- main.js
+++ main.js
@@ -1,19 +1,5 @@
 $(function () {
 
-	new function ($) {
-		$.fn.setCursorPosition = function (pos) {
-			if ($(this).get(0).setSelectionRange) {
-				$(this).get(0).setSelectionRange(pos, pos);
-			} else if ($(this).get(0).createTextRange) {
-				var range = $(this).get(0).createTextRange();
-				range.collapse(true);
-				range.moveEnd('character', pos);
-				range.moveStart('character', pos);
-				range.select();
-			}
-		}
-	} (jQuery);
-
 	$.support.placeholder = (function () {
 		var input = document.createElement('input');
 		return 'placeholder' in input;
